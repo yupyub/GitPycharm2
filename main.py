2826a1b9826c914ded04514c4731fbad1e015c7c
SungYubJung <sungyup5003@gmail.com>
2021-04-09 17:04:22 +0900

    Updates on Local Master Branch

--- main.py
+++ main.py
@@ -17,3 +17,7 @@ print("Hello World 5")
 
 # updates in main branch of Github
 print("Hello World Update on Github branch")
+
+
+# adding new item in master branch
+print("Hello World Updates on Local Master Branch")
