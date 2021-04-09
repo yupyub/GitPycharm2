3d18c2c30470335bd9551d7f6af1202d9545b88d
SungYubJung <sungyup5003@gmail.com>
2021-04-09 17:07:24 +0900

    Update 2 on Github branch Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Update on Github branch")
 
 # adding new item in master branch
 print("Hello World Updates on Local Master Branch")
+
+# updates in main branch of Github
+print("Hello World Update on Github branch Only")
